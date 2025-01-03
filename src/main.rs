76ff12e016c9dd0d33bb6e1e7b9ab03d8bdb0dcd
daichi-miura-ag <daichi.miura.ag@gmail.com>
2025-01-03 19:20:10 +0900

    example: struct/impl/fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
+    let rectangle1 = Rectangle { width: 30, height: 50 };
+    let rectangle2 = Rectangle { width: 40, height: 60 };
+    println!("{}, {}, {}",rectangle1.area(), rectangle2.area(), add(rectangle1.area(), rectangle2.area()));
+}
+
+// structure; 構造体
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
+
+// implementation; 実装
+impl Rectangle {
+    fn area(&self) -> i32 {
+        self.width * self.height
+    }
+}
+
+// function; 関数
+fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
