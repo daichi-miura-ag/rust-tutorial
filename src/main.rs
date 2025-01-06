f85b0eef8338ebd4ff69823aa07341cc4df99e87
daichi-miura-ag <daichi.miura.ag@gmail.com>
2025-01-06 23:22:17 +0900

    example: closure/box

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 fn main() {
     println!("Hello, world!");
+
     let rectangle1 = Rectangle { width: 30, height: 50 };
     let rectangle2 = Rectangle { width: 40, height: 60 };
     println!("{}, {}, {}",rectangle1.area(), rectangle2.area(), add(rectangle1.area(), rectangle2.area()));
+
     let mut numbers = vec![1, 2, 3, 4, 5];
     numbers.push(6);
     println!("{:?}", numbers);
     for num in numbers.iter() {
         println!("{}", num);
     }
+
+    let add_closure = |x: i32, y: i32| x + y;
+    println!("{}", add_closure(5, 7));
+
+    let boxed_n = Box::new(5);
+    println!("{}", boxed_n);
 }
 
 // structure; 構造体
